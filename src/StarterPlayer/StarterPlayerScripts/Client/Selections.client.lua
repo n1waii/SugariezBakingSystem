1a1a3ef198e25fcc498a1b5bb592580ed97d3219
Alphexus <52839154+Alphexus@users.noreply.github.com>
2020-06-24 17:21:05 -0400

    Update Selections.client.lua

--- src/StarterPlayer/StarterPlayerScripts/Client/Selections.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/Client/Selections.client.lua
@@ -38,11 +38,12 @@ local Recipes = require(SharedModules.Recipes)
 
 local Selectables = {}
 
-local CanSelect = true
 local InRange = false
+local ViewingOptions = false
 local LastSelectable = nil
 
 local ButtonDownEffect = false
+local TweeningInteractive = false
 
 local RANGE = 7
 local SELECTION_COOLDOWN = 0.5
@@ -52,7 +53,7 @@ local KEY = Enum.KeyCode.E
 local function GetClosestSelectable() -- // Get array of selectables in range 
 	local InRange = {}
 	for _,v in pairs(Selectables) do
-		if v.Selectable.Occupied.Value == true then continue end
+		if v.Selectable.Occupied.Value then continue end
 		local mag = (v.PrimaryPart.Position-Player.Character.HumanoidRootPart.Position).Magnitude
 		if (mag <= RANGE) and not inAction and not KitchenFolder.inAction.Value then
 			table.insert(InRange, {Model = v, Mag = mag})			
@@ -79,11 +80,75 @@ local function SetupSelectables() --// Sorts all selectables into a table
 	end
 end
 
+local function CloseInteractive()
+	InRange = false
+	LastSelectable = nil
+	local t = TweenService:Create(
+		InteractionKey.Main.Interactive.UIScale, 
+		TweenInfo.new(0.1, Enum.EasingStyle.Linear),
+		{
+			Scale = 0
+		}
+	)
+	TweeningInteractive = true
+	t:Play()
+	t.Completed:Connect(function() 
+		InteractionKey.Adornee = nil
+		TweeningInteractive = false
+	end)
+end
+
+local function CreateOptions(optionsDict)
+	if ViewingOptions then print("already viewing options") return end
+	ViewingOptions = true
+	InteractionKey.Main.Options.UIScale.Scale = 0
+	InteractionKey.Main.Interactive.UIScale.Scale = 0
+
+	for name, callback in pairs(optionsDict) do
+		local Option = InteractionKey.OptionTemplate:Clone()
+		Option.Tween.Text = name
+		Option.Parent = InteractionKey.Main.Options
+		Option.Visible = true
+		Option.MouseButton1Click:Connect(callback)
+		print("created option ", name)
+	end
+
+	-- // Elastic Effect \\ --
+	TweenService:Create(
+		InteractionKey.Main.Options.UIScale, 
+		TweenInfo.new(0.5, Enum.EasingStyle.Elastic, Enum.EasingDirection.Out),
+		{
+			Scale = 1
+		}
+	):Play()
+	print("tweened")
+end
+
+local function DestroyOptions()
+	local t = TweenService:Create(
+		InteractionKey.Main.Options.UIScale, 
+		TweenInfo.new(0.1, Enum.EasingStyle.Linear),
+		{
+			Scale = 0
+		}
+	)
+	t:Play()
+	t.Completed:Connect(function()
+		for _,option in pairs(InteractionKey.Main.Options:GetChildren()) do
+			if option.Name == "OptionTemplate" then
+				option:Destroy()
+				print("Destroyed option")
+			end
+		end
+		ViewingOptions = false
+	end)
+end
+
 -- // Main \\ --
 SetupSelectables()
 
 UserInputService.InputBegan:Connect(function(Input, GameProcessed) -- // On Key pressed
-	if GameProcessed then return end
+	if GameProcessed or TweeningInteractive then return end
 	if Input.KeyCode == KEY then
 		local closestSelectable = GetClosestSelectable()
 		if closestSelectable then
@@ -96,10 +161,10 @@ UserInputService.InputBegan:Connect(function(Input, GameProcessed) -- // On Key 
 			if not ButtonDownEffect then
 				ButtonDownEffect = true
 				local ButtonDownTween = TweenService:Create(
-					InteractionKey.Main.Tween,
+					InteractionKey.Main.Interactive.Label,
 					TweenInfo.new(0.3, Enum.EasingStyle.Quint, Enum.EasingDirection.In, 0, true),
 					{
-						Position = UDim2.fromScale(0.3, 0.3)
+						Position = UDim2.fromScale(0, 0)
 					}
 				)
 				ButtonDownTween:Play()
@@ -118,35 +183,33 @@ RunService.RenderStepped:Connect(function() -- // Selectable effect handler
 
 	local closestSelectable = GetClosestSelectable()
 	if closestSelectable then -- // Tween In
-		InteractionKey.Adornee = closestSelectable
+		InteractionKey.Adornee = closestSelectable.PrimaryPart
 		if not InRange or LastSelectable ~= closestSelectable then
+			if ViewingOptions then
+				print("Destroyed!")
+				DestroyOptions()
+			end
 			LastSelectable = closestSelectable
 			InRange = true
 			Sound:Add(424002310, 1, 1)
-			InteractionKey.Main.UIScale.Scale = 0
-			TweenService:Create(
-				InteractionKey.Main.UIScale, 
+			InteractionKey.Main.Interactive.UIScale.Scale = 0
+			local t = TweenService:Create(
+				InteractionKey.Main.Interactive.UIScale, 
 				TweenInfo.new(0.5, Enum.EasingStyle.Elastic, Enum.EasingDirection.Out),
 				{
 					Scale = 1
 				}
-			):Play()
+			)
+			TweeningInteractive = true
+			t:Play()
+			t.Completed:Connect(function()
+				TweeningInteractive = false
+			end)
 		end
 	else -- // Tween Out
-		if InRange then
-			InRange = false
-			LastSelectable = nil
-			local t = TweenService:Create(
-				InteractionKey.Main.UIScale, 
-				TweenInfo.new(0.1, Enum.EasingStyle.Linear),
-				{
-					Scale = 0
-				}
-			)
-			t:Play()
-			t.Completed:Connect(function() 
-				InteractionKey.Adornee = nil
-			end)
+		if InRange then		
+			DestroyOptions()
+			CloseInteractive()
 		end
 	end
 end)
@@ -171,6 +234,22 @@ SelectionBindables.Vanilla.Event:Connect(function()
 	if ToolFunctions.FindToolFromName(Tools, "Vanilla Extract") == nil then
 		KitchenRemotes.GiveVanilla:FireServer()
 	end
+end)
+
+SelectionBindables.OvenOptions.Event:Connect(function()
+	local OptionsDict = {
+		["Stove"] = function()
+			DestroyOptions()
+		end,
+
+		["Oven"] = function()
+			DestroyOptions()
+		end
+	}
+
+
+	CreateOptions(OptionsDict)
+	print("Created options")
 end)
 
 SelectionBindables.Mix.Event:Connect(function(Selected)
